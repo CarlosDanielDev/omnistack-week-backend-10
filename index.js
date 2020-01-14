13d80d81bc001eece57f2cabbf324ade5156dcd8
DanPHP7 <killer.dk20@gmail.com>
2020-01-14 10:09:43 -0300

    comentarios uteis

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ import express from "express";
 
 const app = express();
 
-// GET POST, PUT, DELETE
+app.use(express.json()); // informei ao express pra usar JSON no corpo da requisição.
+// Métodos HTTP: GET POST, PUT, DELETE
+
+// Tipos de parâmetros:
+// Query Params: req.query (filtros, ordenação, paginação)
+// Route Params: req.params (Identificar recurso na alteração ou remoção);
+// Body: req.body (Dado para criação, ou alteração de registros)
 
 app.get("/", (req, res) => {
   return res.json({ hello: "World" });
